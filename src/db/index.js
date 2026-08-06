779f68771ac0fc0eb998b4f72b746b487d2437c7
itsme-umar <farooq8577@gmail.com>
2026-08-07 00:25:54 +0530

    added utils , .npmrc , connected DB , intialised app from express

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../contants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MONGODB is connected!! DB HOST: ${connectionInstance}`)
+    } catch (error) {
+        console.error(`MONGODB Connection FAILED: ${error}`);
+        process.exit(1);
+        throw error;
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+import app from "./app.js";
+
+dotenv.config({ path: './.env' })
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+    .then(() => {
+        app.on('error', (err) => {
+            console.log('Server connection ERROR:', err)
+            throw err
+        })
+        app.listen(PORT, () => {
+            console.log('Server is started on PORT', PORT);
+
+        })
+    })
+    .catch((err) => {
+        console.log('MONGODB connection failes!!!', err);
+
+    })
