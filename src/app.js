3869a78bcf16e0f8af362526150c95326a3dc886
itsme-umar <farooq8577@gmail.com>
2026-08-12 23:11:04 +0530

    created dummy controller and route for the user/register

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-}))
+  })
+);
 
-app.use(express.json({ limit: '16kb' }))
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
-app.use(express.static('public'))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Routes imports
+
+import userRouter from "./routes/user.route.js";
+
+// Route Declaration
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
